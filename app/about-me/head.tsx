e405135369f9810a59c777115b9d8adf4ba07d11
root <root@LAPTOP-TGJ12C00>
2022-11-09 20:42:53 +0700

    wip: fix display glitch for mobile

---
+++ app/about-me/head.tsx
@@ -0,0 +1,10 @@
+export default async function Head() {
+  return (
+    <>
+      <title>About Me</title>
+      <meta name="description" content="Me myself and I" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
